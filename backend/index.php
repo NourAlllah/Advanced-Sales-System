31fbf6160e0b6f0ba60fcdd02b676388104a08c0
NourAllah <nourabase1998@gmail.com>
2025-04-22 00:05:33 +0200

    setting up routes

--- backend/index.php
+++ backend/index.php
@@ -4,16 +4,16 @@ require_once 'db.php';
 
 // Get request method and path
 $method = $_SERVER['REQUEST_METHOD'];
-$uri = dirname($_SERVER['SCRIPT_NAME']); 
-//echo json_encode([$uri]);
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = str_replace('/Advanced-Sales-System/backend', '', $uri); 
 
 // Router
 if ($method === 'POST' && $uri === '/orders') {
-    require 'routes/post-order.php';
+    require 'post-order.php';
 } elseif ($method === 'GET' && $uri === '/analytics') {
-    require 'routes/get-analytics.php';
+    require 'get-analytics.php';
 } elseif ($method === 'GET' && $uri === '/recommendations') {
-    require 'routes/get-recommendations.php';
+    require 'get-recommendations.php';
 } else {
     http_response_code(404);
     echo json_encode(['error' => 'Endpoint not found']);
